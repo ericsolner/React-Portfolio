33a28f9f46f3c41bfb705ac9a3ffe4ae76c5ed2b
Eric Solner <ericsolner2018@u.northwestern.edu>
2018-06-02 16:27:50 -0500

    "added projects"

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //import {Route, Switch } from 'react-router-dom';
 import Nav from '../nav';
 import Footer from '../footer';
+import Projects from './pages/projects';
 import Skills from './pages/skills';
 import About from './pages/about';
 
@@ -15,6 +16,7 @@ class Main extends Component {
         <About/>
         <Nav/>
         <Skills/>
+        <Projects/>
       </main>
     );
   }
