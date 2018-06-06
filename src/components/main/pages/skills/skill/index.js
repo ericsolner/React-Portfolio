150fdcc23a1242a84adf66d47cd1478193ae86f2
Steven Daoud <steven.daoud@gmail.com>
2018-06-05 21:48:47 -0500

    Added fade ins to skills

--- src/components/main/pages/skills/skill/index.js
+++ src/components/main/pages/skills/skill/index.js
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
 import ReactSVG from 'react-svg';
+import Waypoint from 'react-waypoint';
 
 class Skill extends Component {
+ constructor(){
+   super();
+   this.state = {
+     ready : false
+   };
+
+   this.showSkill = this.showSkill.bind(this);
+ }
+
+ showSkill(){
+    this.setState({
+      ready : true
+    });
+ }
 
  render() {
   return (
-    <div className="skill">
+    <div className={`skill ${this.state.ready ? 'loaded' : '' }`}>
         <div className="skill-wrap">
            <ReactSVG path={require(`../../../../../assets/img/skills-icons/${this.props.data.image}`)}
         />
         <h3>{this.props.data.title}</h3>
+        </div>
+
+        <div className="position-trigger">
+          <Waypoint
+            onEnter={this.showSkill}
+          />
         </div>
     </div>
   )
